32dcd47af6040db0bd4f29c48efb913f56b0e14a
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-10 12:26:09 -0400

    Day 1; BGR thresholding

--- src/Vision.java
+++ src/Vision.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 import stuyvision.VisionModule;
 import stuyvision.gui.VisionGui;
+import stuyvision.gui.IntegerSliderVariable;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -9,7 +10,19 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 public class Vision extends VisionModule {
+    public IntegerSliderVariable minRed = new IntegerSliderVariable("Min Red", 0,  0, 255);
+    public IntegerSliderVariable maxRed = new IntegerSliderVariable("Max Red", 70, 0, 255);
+
     public void run(Mat frame) {
         postImage(frame, "Camera Feed");
+
+        ArrayList<Mat> channels = new ArrayList<Mat>();
+        Core.split(frame, channels);
+
+        Mat redChannel = channels.get(2);
+
+        Core.inRange(redChannel, new Scalar(minRed.value()), new Scalar(maxRed.value()), frame);
+
+        postImage(frame, "Filtered Frame");
     }
 }
