541cbc1a07f81187748ab780af88eb0a45d69fd3
Kevin, Winston <panda501wp@gmail.com>
2017-03-22 18:37:45 -0400

    Make HSV filter filter color

--- src/Vision.java
+++ src/Vision.java
@@ -11,49 +11,54 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import stuyvision.gui.IntegerSliderVariable;
 import stuyvision.gui.IntegerSliderVariable;
+
 public class Vision extends VisionModule {
-	public IntegerSliderVariable minHue = new IntegerSliderVariable("Min Hue", 110, 0, 255);
-	public IntegerSliderVariable maxHue = new IntegerSliderVariable("Max Hue", 116, 0, 255);
-	public IntegerSliderVariable minSat = new IntegerSliderVariable("Min Sat", 100, 0, 255);
-	public IntegerSliderVariable maxSat = new IntegerSliderVariable("Max Sat", 101, 0, 255);
-	public IntegerSliderVariable minVal = new IntegerSliderVariable("Min Val", 101, 0, 255);
-	public IntegerSliderVariable maxVal = new IntegerSliderVariable("Max Val", 101, 0, 255);
+	
+	public IntegerSliderVariable minHue = new IntegerSliderVariable("Min Hue", 101, 0, 255);
+	public IntegerSliderVariable maxHue = new IntegerSliderVariable("Max Hue", 143, 0, 255);
+	public IntegerSliderVariable minSat = new IntegerSliderVariable("Min Sat", 179, 0, 255);
+	public IntegerSliderVariable maxSat = new IntegerSliderVariable("Max Sat", 237, 0, 255);
+	public IntegerSliderVariable minVal = new IntegerSliderVariable("Min Val", 255, 0, 255);
+	public IntegerSliderVariable maxVal = new IntegerSliderVariable("Max Val", 255, 0, 255);
 	
 	public void run(Mat frame) {
     	
         postImage(frame, "Camera Feed");
-
         Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
 
         ArrayList<Mat> channels = new ArrayList<Mat>();
 
         Core.split(frame, channels);
-
-        postImage(channels.get(0), "Hue channel");
+        //postImage(channels.get(0), "Hue channel");
 
         Mat filterHue = new Mat();
-
         Core.inRange(channels.get(0), new Scalar(minHue.value()), new Scalar(maxHue.value()), filterHue);
-        
         postImage(filterHue, "Darwinism");
         
         Mat filterSat = new Mat();
-        
         Core.inRange(channels.get(1), new Scalar(minSat.value()), new Scalar(maxSat.value()), filterSat);
-        
         postImage(filterSat, "Bryan has no Friends");
         
         Mat filterVal = new Mat();
         Core.inRange(channels.get(2), new Scalar(minVal.value()), new Scalar(maxVal.value()), filterVal);
         postImage(filterVal, "Glados");
         
-        Mat filterupdate = new Mat();
+        Mat filterUpdate = new Mat();
         
-        Core.bitwise_and(filterVal, filterSat, filterupdate);
+        Core.bitwise_and(filterVal, filterSat, filterUpdate);
+        Core.bitwise_and(filterUpdate, filterHue, filterUpdate);
+        postImage(filterUpdate, "HSV filter");
         
-        Core.bitwise_and(filterupdate, filterHue, filterupdate);
+        Mat HSVFilterNColor = new Mat();
+        ArrayList<Mat> filterUpdateSandwich = new ArrayList<Mat>();
         
-        postImage(filterupdate, "HSV filter");
-        
+        filterUpdateSandwich.add(filterUpdate);
+        filterUpdateSandwich.add(filterUpdate);
+        filterUpdateSandwich.add(filterUpdate);
+        Core.merge(filterUpdateSandwich, filterUpdate);
+        Imgproc.cvtColor(filterUpdate,filterUpdate, Imgproc.COLOR_GRAY2BGR);
+        Imgproc.cvtColor(filterUpdate,filterUpdate, Imgproc.COLOR_BGR2HSV);
+        Core.bitwise_and(filterUpdate, frame, HSVFilterNColor);
+        postImage(HSVFilterNColor, "Racist HSV");
     }
 }
