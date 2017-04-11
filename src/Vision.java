6dbe8dd064e4efcbcd4bae18c619ca5ef307df06
huiminthehuman <huiminwu24@gmail.com>
2017-04-11 10:30:51 -0400

    Add code used for learning how to erode, dilate, merge,  and blur.

--- src/Vision.java
+++ src/Vision.java
@@ -13,5 +13,33 @@ import org.opencv.imgproc.Imgproc;
 public class Vision extends VisionModule {
     public void run(Mat frame) {
         postImage(frame, "Camera Feed");
-    }
+   	
+	Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
+
+	ArrayList<Mat> channels = new ArrayList<Mat>();
+	Core.split(frame, channels);
+	postImage(channels.get(0), "Hui Channel");
+	postImage(channels.get(1), "Saturation Channel");
+	postImage(channels.get(2), "Value Channel");
+	
+	Mat merged = new Mat();
+	Core.merge(channels, merged);
+	Imgproc.cvtColor(merged, merged, Imgproc.COLOR_HSV2BGR);
+	postImage(merged, "Merged channel");
+   	//should be outputing the same form as CameraFeed
+	
+	//should dilate
+	Mat dilateKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+	Imgproc.dilate(channels.get(0), channels.get(0), dilateKernel);
+	postImage(channels.get(0), "Dilated hui");
+	
+	//should erode
+	Mat erodeKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(7, 7));
+	Imgproc.erode(channels.get(0), channels.get(0), erodeKernel);
+	postImage(channels.get(0), "Eroded hui");
+
+	//should blur the merged frame
+	Imgproc.medianBlur(merged, merged, 5);
+	postImage(merged, "Merged RGB channel");
+   }
 }
